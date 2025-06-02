a4f4172a35fb89b5b8fefc65ecc9f7febde9b1f6
Guilherme Chaves Sena <114080615+GuiiiChaves@users.noreply.github.com>
2025-06-02 13:41:46 -0300

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,35 +9,35 @@ import { useNavigate } from 'react-router-dom';
 
 const avatarOptions = [
   { 
-    id: 'cyber-girl', 
-    image: 'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?w=100&h=100&fit=crop&crop=face',
+    id: 'cyber-bot', 
+    image: 'https://img.freepik.com/vetores-premium/robo-bonito-icon-ilustracao-conceito-de-icone-de-robo-de-tecnologia-isolado-estilo-cartoon-plana_138676-1219.jpg',
     label: 'Cyber' 
   },
   { 
-    id: 'mystic-scholar', 
-    image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100&h=100&fit=crop&crop=face',
-    label: 'Místico' 
+    id: 'mystic', 
+    image: 'https://cdn-icons-png.flaticon.com/512/2793/2793202.png',
+    label: 'Mago' 
   },
   { 
     id: 'urban-explorer', 
-    image: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=100&h=100&fit=crop&crop=face',
+    image: 'https://cdn-icons-png.flaticon.com/512/5230/5230440.png
     label: 'Explorador' 
   },
   { 
     id: 'creative-mind', 
-    image: 'https://images.unsplash.com/photo-1494790108755-2616b612b1e5?w=100&h=100&fit=crop&crop=face',
-    label: 'Criativa' 
+    image: 'https://img.freepik.com/vetores-premium/uma-rapariga-bonita-vestida-de-detective-com-uma-lupa-de-vetor-de-desenho-animado_1080480-51566.jpg?semt=ais_hybrid&w=740',
+    label: 'Detetive' 
   },
   { 
     id: 'wise-guardian', 
-    image: 'https://images.unsplash.com/photo-1566492031773-4f4e44671d66?w=100&h=100&fit=crop&crop=face',
-    label: 'Guardião' 
+    image: 'https://cdn-icons-png.flaticon.com/512/2278/2278606.png',
+    label: 'Jogador' 
   },
 ];
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
-  const [selectedAvatar, setSelectedAvatar] = useState('cyber-girl');
+  const [selectedAvatar, setSelectedAvatar] = useState('cyber-bot');
   const [loginData, setLoginData] = useState({
     email: '',
     password: ''
